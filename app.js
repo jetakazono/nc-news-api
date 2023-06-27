26f0238e6d1f1cb8f37de76726bcd9b874fd9b7f
jetakazono <jessica.takazono@gmail.com>
2023-06-27 01:54:10 +0100

    nc-news-4 add articles endpoint + tests and errors handlers

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ const express = require("express")
 
 const { getEndpoints } = require("./controllers/api.controllers")
 const { getAllTopics } = require("./controllers/topics.controller")
-const { handleServerErrors } = require("./errors/errors")
+const { getArticleById } = require("./controllers/articles.controller")
+const {
+    handleServerErrors,
+    handlePsqlErrors,
+    handleCustomErrors,
+} = require("./errors/errors")
 
 const app = express()
 
@@ -10,9 +15,15 @@ app.get("/api", getEndpoints)
 
 app.get("/api/topics", getAllTopics)
 
+app.get("/api/articles/:article_id", getArticleById)
+
 app.all("*", (_, res) => {
     res.status(404).send({ msg: "not found" })
 })
+
+app.use(handleCustomErrors)
+
+app.use(handlePsqlErrors)
 
 app.use(handleServerErrors)
 
