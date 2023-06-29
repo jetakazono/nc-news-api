207f0f36c5bf91b445358db857f5bb6dd0401719
jetakazono <jessica.takazono@gmail.com>
2023-06-29 13:54:52 +0100

    set up host application

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,12 @@
 const { Pool } = require("pg")
 const ENV = process.env.NODE_ENV || "development"
+const config =
+    ENV === "production"
+        ? {
+              connectionString: process.env.DATABASE_URL,
+              max: 2,
+          }
+        : {}
 
 require("dotenv").config({
     path: `${__dirname}/../.env.${ENV}`,
@@ -8,13 +15,5 @@ require("dotenv").config({
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
     throw new Error("PGDATABASE or DATABASE_URL not set")
 }
-
-const config =
-    ENV === "production"
-        ? {
-              connectionString: process.env.DATABASE_URL,
-              max: 2,
-          }
-        : {}
 
 module.exports = new Pool(config)
